df5eb4162ac6f2881752399449ccdcbb5c66e5b7
distortia <nickstalter@gmail.com>
2014-11-06 13:01:55 -0500

    merging dev to master

--- includes/header.php
+++ includes/header.php
@@ -6,12 +6,12 @@
 		<title>Coach Tech RV Services - Ottawa Lake, MI</title>
 		<meta name="generator" content="Bootply" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-		<link href="/bstheme/css/bootstrap.min.css" rel="stylesheet">
+		<link href="/coachtech/css/bootstrap.min.css" rel="stylesheet">
 		<link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 		<!--[if lt IE 9]>
 			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
-		<link href="css/styles.css" rel="stylesheet">
+		<link href="/coachtech/css/styles.css" rel="stylesheet">
 	</head>
 	<body>
 <nav class="navbar navbar-trans navbar-fixed-top" role="navigation">
